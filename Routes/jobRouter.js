6eade796edad9a3a83023c81c46e3462b052fd30
durgadevivk <vkdurgadevikrishnan@gmail.com>
2026-09-11 13:00:35 +0530

    add job routes from main to dev

---
+++ Routes/jobRouter.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const {getAllJobs, getJobById, createJob, updateJob, deleteJob, getRecruiterJobs, getJobApplications}=require('../Controllers/jobController');
+const { isAuthenticated, allowRoles } = require('../middlewares/auth');
+const jobRouter=express.Router();
+//public routes
+jobRouter.get('/',getAllJobs);
+jobRouter.get('/:id',getJobById)
+//protected routes
+jobRouter.post('/',isAuthenticated,allowRoles(['recruiter']),createJob);
+jobRouter.put('/:id',isAuthenticated,allowRoles(['recruiter']),updateJob);
+jobRouter.delete('/:id',isAuthenticated,allowRoles(['recruiter']),deleteJob);
+jobRouter.get('/recruiter/jobs',isAuthenticated,allowRoles(['recruiter']),getRecruiterJobs);
+jobRouter.get('/recruiter/jobs/:id/applications',isAuthenticated,allowRoles(['recruiter']),getJobApplications);
+
+
+module.exports=jobRouter;
